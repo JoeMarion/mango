d301dec7d32897f156e4cd22dc256cc5da41308a
Joe Marion <joemarion.io@gmail.com>
2017-12-06 18:08:21 -0600

    Create Product and Catalog modules.

---
+++ lib/mango/catalog/catalog.ex
@@ -0,0 +1,9 @@
+defmodule Mango.Catalog do
+  alias Mango.Catalog.Product
+
+  def list_products do
+    product1 = %Product{ name: "Tomato", price: 50 }
+    product2 = %Product{ name: "Apple", price: 100 }
+    [product1, product2]
+  end
+end
---
+++ test/mango/catalog/catalog_test.exs
@@ -0,0 +1,12 @@
+defmodule Mango.CatalogTest do
+  use ExUnit.Case
+  alias Mango.Catalog
+  alias Mango.Catalog.Product
+
+  test "list_products/0 returns all products" do
+    [p1 = %Product{}, p2 = %Product{}] = Catalog.list_products
+
+    assert p1.name ==  "Tomato"
+    assert p2.name ==  "Apple"
+  end
+end
