46abf11ca3053029887a8a7611b517363364117c
Joe Marion <joemarion.io@gmail.com>
2017-12-08 00:06:11 -0600

    Add show and filter for categories.

--- test/mango/catalog/catalog_test.exs
+++ test/mango/catalog/catalog_test.exs
@@ -9,4 +9,16 @@ defmodule Mango.CatalogTest do
     assert p1.name ==  "Tomato"
     assert p2.name ==  "Apple"
   end
+
+  test "list_seasonal_products/0 returns all seasonal products" do
+    [product = %Product{}] = Catalog.list_seasonal_products
+
+    assert product.name == "Apple"
+  end
+
+  test "get_category_products/1 returns products of the given category" do
+    [product = %Product{}] = Catalog.get_category_products("fruits")
+
+    assert product.name == "Apple"
+  end
 end
